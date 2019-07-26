fe56f481ad133c0746ec839198646fee48bad170
rsaad3 <rabea.sa3ad@dxc.com>
2019-07-26 16:03:10 +0200

    convert to war

--- src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -2,11 +2,19 @@ package com.javainuse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class SpringBootHelloWorldApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringBootHelloWorldApplication.class);
 	}
 }
